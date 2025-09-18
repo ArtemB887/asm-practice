695549bc0b9df7239dcd4163fea2832c9eb257f0
ArtemB887 <artem.burdun@e-u.edu.ua>
2025-09-18 18:50:16 +0300

    Update hw2.asm

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -1,13 +1,10 @@
 section .data
-    ; тут можна залишити порожньо або додати повідомлення
 section .bss
-    buffer  resb 20        ; буфер для рядка
+    buffer  resb 20
 
 section .text
     global _start
 
-; EAX - число, яке конвертуємо
-; ESI - адреса буфера
 int2str:
     push eax
     mov ecx, 0
@@ -15,9 +12,9 @@ int2str:
 .next_digit:
     xor edx, edx
     mov ebx, 10
-    div ebx                 ; ділимо EAX на 10, залишок у EDX
-    add dl, '0'             ; цифру перетворюємо в ASCII
-    push dx                 ; зберігаємо цифру у стек
+    div ebx
+    add dl, '0'
+    push dx
     inc ecx
     test eax, eax
     jnz .next_digit
@@ -28,24 +25,21 @@ int2str:
     inc esi
     loop .write_digits
 
-    mov byte [esi], 0       ; закінчуємо рядок нуль-байтом
+    mov byte [esi], 0
     pop eax
     ret
 
-; -----------------------
 _start:
     mov eax, 1234567
     mov esi, buffer
     call int2str
 
-    ; sys_write для перевірки
     mov eax, 4
     mov ebx, 1
     mov ecx, buffer
     mov edx, 20
     int 0x80
 
-    ; завершення програми
     mov eax, 1
     xor ebx, ebx
     int 0x80
